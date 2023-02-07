599772343f06b61813bf45b56bb626b34dc8c439
gwolak <grzegorzwolak97@gmail.com>
2023-02-07 11:44:56 +0100

    added switch to equal function

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const actual = document.querySelector('.calculator__output-actual');
 const clearBtn = document.querySelector('.clear');
 const backBtn = document.querySelector('.back');
 const calcBtns = document.querySelectorAll('.calc-btn');
-const operationBtns = document.querySelectorAll('.operation')
+const operationBtns = document.querySelectorAll('.operation');
 const equalBtn = document.querySelector('.equal');
-const changeBtn = document.querySelector('.change')
+const changeBtn = document.querySelector('.change');
 
 // first part of operation variable
 let x = 0;
@@ -15,87 +15,90 @@ let operation;
 const clearArea = () => {
 	input.textContent = '0';
 	actual.textContent = '';
-    x = 0;
-    y = 0;
-    operation = ''
-    checkLength()
+	x = 0;
+	y = 0;
+	operation = '';
+	checkLength();
 };
 
 const backSpace = () => {
 	input.textContent = input.textContent.slice(0, -1);
-    y = parseFloat(input.textContent)
-    checkLength()
+	y = parseFloat(input.textContent);
+	checkLength();
 };
 
 const typeActual = (el) => {
-    if (input.textContent.length > 10) {
-        return
-    }
+	if (input.textContent.length > 10) {
+		return;
+	}
 	if (el.textContent !== '.' && input.textContent === '0') {
 		input.textContent = '';
 	}
 	if (input.textContent.indexOf('.') !== -1 && el.textContent === '.') {
 		return;
 	}
-    if (actual.textContent.indexOf('=') !== -1) {
-        input.textContent = ''
-        actual.textContent = ''
-    }
-    
+	if (actual.textContent.indexOf('=') !== -1) {
+		input.textContent = '';
+		actual.textContent = '';
+	}
 
 	input.textContent = input.textContent + el.textContent;
-    y = parseFloat(input.textContent)
-    checkLength()
-
+	y = parseFloat(input.textContent);
+	checkLength();
 };
 
 const checkLength = () => {
-    if (input.textContent.length > 8 || actual.textContent.length > 8) {
-        input.style.fontSize = '16px'
-        actual.style.fontSize = '10px'
-    } else {
-        input.style.fontSize = '30px'
-        actual.style.fontSize = '14px'
-    }
-}
+	if (input.textContent.length > 8 || actual.textContent.length > 8) {
+		input.style.fontSize = '16px';
+		actual.style.fontSize = '10px';
+	} else {
+		input.style.fontSize = '30px';
+		actual.style.fontSize = '14px';
+	}
+};
 
 const changeChar = () => {
-    if (input.textContent === '0') {
-        return
-    }
-    else if (input.textContent.charAt(0) === '-') {
-        input.textContent = input.textContent.substring(1)
-    } else {
-        input.textContent = '-' + input.textContent
-    }
+	if (input.textContent === '0') {
+		return;
+	} else if (input.textContent.charAt(0) === '-') {
+		input.textContent = input.textContent.substring(1);
+	} else {
+		input.textContent = '-' + input.textContent;
+	}
 
-    y = parseFloat(input.textContent)
-    
-}
+	y = parseFloat(input.textContent);
+};
 
 const startCalc = (e) => {
 	x = parseFloat(input.textContent);
 	actual.textContent = `${input.textContent} ${e}`;
-    input.textContent = '0';
-    operation = e
+	input.textContent = '0';
+	operation = e;
 };
 
 const equal = () => {
-    if (operation === '+') {
-        input.textContent = ((x + y).toFixed(10) * 100) / 100
-        actual.textContent = `${x} + ${y} =`
-    } else if (operation === '-') {
-        input.textContent = ((x - y).toFixed(10) * 100) / 100
-        actual.textContent = `${x} - ${y} =`
-    } else if (operation === '*') {
-        input.textContent = ((x * y).toFixed(10) * 100) / 100
-        actual.textContent = `${x} * ${y} =`
-    } else if (operation === '/') {
-        input.textContent = ((x / y).toFixed(10) * 100) / 100
-        actual.textContent = `${x} / ${y} =`
-    } 
-    checkLength()
-}
+
+	switch (operation) {
+		case '-':
+			input.textContent = ((x - y).toFixed(10) * 100) / 100;
+			actual.textContent = `${x} - ${y} =`;
+			break;
+		case '*':
+			input.textContent = ((x * y).toFixed(10) * 100) / 100;
+			actual.textContent = `${x} * ${y} =`;
+			break;
+		case '/':
+			input.textContent = ((x / y).toFixed(10) * 100) / 100;
+			actual.textContent = `${x} / ${y} =`;
+			break;
+        default:
+			input.textContent = ((x + y).toFixed(10) * 100) / 100;
+			actual.textContent = `${x} + ${y} =`;
+    }
+			
+
+	checkLength();
+};
 
 clearBtn.addEventListener('click', clearArea);
 backBtn.addEventListener('click', backSpace);
@@ -104,10 +107,10 @@ calcBtns.forEach((el) => {
 		typeActual(el);
 	});
 });
-operationBtns.forEach(el => {
-    el.addEventListener('click', (e) => {
-        startCalc(e.target.textContent);
-    })
-})
-changeBtn.addEventListener('click', changeChar)
-equalBtn.addEventListener('click', equal)
+operationBtns.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		startCalc(e.target.textContent);
+	});
+});
+changeBtn.addEventListener('click', changeChar);
+equalBtn.addEventListener('click', equal);
